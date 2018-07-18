648091a9a1ab31a93377c57b11d9307b96fbad9f
Overlord Yasha <darkyashamaru@gmail.com>
2018-07-18 14:02:24 -0500

    Sprites, Background
    Sprites for hazards and background for the scene

---
+++ Jumping Jack/Assets/Scripts/Gameplay/HazardInstancer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardInstancer : MonoBehaviour {
+    public GameObject[] HazardModels;
+    public static int HazardIndex;
+
+    private void Awake()
+    {
+        Instance();
+    }
+
+    void Instance()
+    {
+        GameObject instance = Instantiate(HazardModels[HazardIndex], transform);
+        instance.transform.localPosition = Vector3.zero;
+        IncreaseIndex();
+    }
+
+    void IncreaseIndex ()
+    {
+        HazardIndex++;
+        if(HazardIndex >= HazardModels.Length-1)
+        {
+            HazardIndex = 0;
+        }
+    }
+
+}
---
+++ Jumping Jack/Assets/Scripts/General/MaterialScroller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialScroller : MonoBehaviour {
+    public float ScrollSpeed = 1;
+    public float currentValue;
+    Material mat;
+
+    private void Awake()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        currentValue += ScrollSpeed * Time.deltaTime;
+        mat.SetTextureOffset("_MainTex", new Vector2(currentValue, 0));
+    }
+}
